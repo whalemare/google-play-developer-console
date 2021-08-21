9c882d3372b46d74fb807d1a3bc0ddda9d923d85
Anton Vlasov <nanostet@gmail.com>
2021-08-21 17:18:32 +0700

    Template

--- src/lib/async.ts
+++ src/lib/async.ts
@@ -18,15 +18,15 @@
  * @returns a Promise which should contain `['a','b','c']`
  */
 export const asyncABC = async () => {
-  const somethingSlow = (index: 0 | 1 | 2) => {
-    const storage = 'abc'.charAt(index);
+  const somethingSlow = async (index: 0 | 1 | 2) => {
+    const storage = 'abc'.charAt(index)
     return new Promise<string>((resolve) =>
       // later...
-      resolve(storage)
-    );
-  };
-  const a = await somethingSlow(0);
-  const b = await somethingSlow(1);
-  const c = await somethingSlow(2);
-  return [a, b, c];
-};
+      resolve(storage),
+    )
+  }
+  const a = await somethingSlow(0)
+  const b = await somethingSlow(1)
+  const c = await somethingSlow(2)
+  return [a, b, c]
+}
